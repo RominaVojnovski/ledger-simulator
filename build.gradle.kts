a04d99dcfd020c76bd0354ae60da46ad407bdbca
Romina Vojnovski <romina.vojnovski@gmail.com>
2025-11-28 00:16:20 -0500

    adding coverage

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    kotlin("jvm") version "1.9.10"
+    java
+    jacoco
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy("jacocoTestReport") // generate report after tests run
+}
+tasks.named<JacocoReport>("jacocoTestReport") {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(files("build/jacoco/test.exec"))
+}
+
+tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(tasks.test)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = 0.8.toBigDecimal()
+            }
+        }
+    }
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    executionData.setFrom(files("build/jacoco/test.exec"))
+}
+
+tasks.check {
+    dependsOn("jacocoTestCoverageVerification")
+}
